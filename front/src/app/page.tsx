f6948b873e7a65e3d4d697abb4d618722c280419
LeeEugene1 <uwm1004@gmail.com>
2023-12-24 16:09:12 +0900

    feat: kakao map

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,26 +1,33 @@
-// "use client";
-import Layout from '@/components/layout'
-import Link from 'next/link'
-import { useEffect } from 'react'
+"use client";
+import Script from 'next/script';
+
+declare global {
+    interface Window {
+        kakao: any;
+    }
+}
+
 
 export default function Home() {
-  // useEffect(()=>{
-  //   getList()
-  // },[])
-  // const getList = async () => {
-  //   const key = 'mlJb3YcgkbdX5HN8aOZgKK2jKset%2FWwZ5m1un7Iq%2BkRG%2BlQ3RWrEDaT5qZ6fyogDmeXRfR28oXh0w7Qxc8tA5Q%3D%3D'
-  //   const res = await fetch('https://api.odcloud.kr/api/15119741/v1/uddi:fe904caf-636f-4a49-aa94-e9064a446b3e?page=1&perPage=10',{
-  //     method: "GET", // *GET, POST, PUT, DELETE, etc.
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Authorization': 'Infuser test-api'
-  //     },
-  //   })
-  //   console.log(res)
-  // }
+    const onLoadFunc = () => {
+        window.kakao.maps.load(function () {
+        const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+            mapOption = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+            level: 3, // 지도의 확대 레벨
+            };
+        // v3가 모두 로드된 후, 이 콜백 함수가 실행됩니다.
+          const map = new window.kakao.maps.Map(mapContainer, mapOption); 
+        });
+    }
   return (
-    <Layout>
-      
-    </Layout>
-  )
+    <>
+      <Script
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP}&autoload=false`}
+        onLoad={()=>onLoadFunc()}
+      ></Script>
+      <div id="map" className="w-full h-screen"></div>
+    </>
+  );
 }
